26e229a95c6b5d71b954bd1e5fc37a229d48e15b
gongzaijuan <18521422204@163.com>
2019-08-15 14:55:43 +0800

    初始化完成

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
-
-Vue.config.productionTip = false
+import '../static/css/reset.css'
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+  el: '#app',
+  render: h => h(App),
+  router
+})
